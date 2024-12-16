a018f9e0ad56618cfac3772f0170f9b097c4526b
Mazzzya <mmyrzya@mail.ru>
2024-12-16 20:56:45 +0600

    Обновление файлов

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let socket;
 let typingTimeout;
 
-// Подключение WebSocket
 function connectWebSocket() {
     socket = new WebSocket('wss://lime-orange-sassafras.glitch.me/ws');
 
@@ -13,7 +12,7 @@ function connectWebSocket() {
     socket.onmessage = (event) => {
         const data = JSON.parse(event.data);
         if (data.type === 'message') {
-            displayMessage(data.username, data.message);
+            displayMessage(data.username, data.message, data.time);
         } else if (data.type === 'typing') {
             displayTypingIndicator(data.username);
         }
@@ -25,20 +24,18 @@ function connectWebSocket() {
     };
 }
 
-// Отправка сообщения
 function sendMessage() {
     const messageInput = document.getElementById('messageInput');
     const message = messageInput.value.trim();
 
     if (message) {
-        const data = { type: 'message', username: 'Гость', message };
+        const data = { type: 'message', username: 'Гость', message, time: new Date().toLocaleTimeString() };
         socket.send(JSON.stringify(data));
         messageInput.value = '';
         clearTypingIndicator();
     }
 }
 
-// Индикация набора сообщения
 function sendTypingStatus() {
     socket.send(JSON.stringify({ type: 'typing', username: 'Гость' }));
     clearTimeout(typingTimeout);
@@ -54,20 +51,17 @@ function displayTypingIndicator(username) {
     indicator.textContent = username ? `${username} набирает сообщение...` : '';
 }
 
-// Отображение сообщений
-function displayMessage(username, message) {
+function displayMessage(username, message, time) {
     const chatBox = document.getElementById('chat-box');
     const msgDiv = document.createElement('div');
-    msgDiv.textContent = `${username}: ${message}`;
+    msgDiv.innerHTML = `<div class="chat-message"><span class="chat-user">${username}</span>: ${message}<span class="chat-time">${time}</span></div>`;
     chatBox.appendChild(msgDiv);
     chatBox.scrollTop = chatBox.scrollHeight;
 }
 
-// Выйти из чата
 function logout() {
     socket.close();
     window.location.href = 'index.html';
 }
 
-// Запуск WebSocket
 connectWebSocket();
